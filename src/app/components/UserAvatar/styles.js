a7e8508d1b2ec2cbbaa0abab57498a6782775189
davidtito1992 <tdavid2000@hotmail.com>
2022-07-29 16:31:28 -0300

    added UserAvatar Component

---
+++ src/app/components/UserAvatar/styles.js
@@ -0,0 +1,35 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container:{
+    flexDirection:'row',
+  },
+  viewContainer:{
+    flexDirection:'row',
+    marginTop:70,
+    paddingHorizontal: 16
+  },
+  descriptionContainer:{
+    flex:1,
+    justifyContent:'center',
+
+  },
+  avatar: {
+    width:40,
+    height:40,
+    borderRadius: 40/2,
+    marginRight: 16
+  },
+  name: {
+    fontSize:14,
+    lineHeight:20,
+    color:'#FFFFFF',
+    fontFamily:'sans-serif'
+  },
+  description: {
+    fontSize:12,
+    lineHeight:14,
+    color:'#FFFFFF',
+    fontFamily:'sans-serif'
+  }
+});
