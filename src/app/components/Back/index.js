3db16a7c46fe31d19a06e77b11439cdec291737d
davidtito1992 <tdavid2000@hotmail.com>
2022-07-29 16:31:05 -0300

    changes in Back Component

---
+++ src/app/components/Back/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  TouchableOpacity,
+  Image
+} from 'react-native';
+import styles from './styles';
+
+const Back = ({ goBack, imageSource }) => {
+  return (
+          <TouchableOpacity style={styles.container} onPress={goBack}>
+              <Image style={styles.backImage} source={imageSource}/>
+          </TouchableOpacity>
+  );
+};
+
+export default Back;
---
+++ src/app/components/Back/styles.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container:{
+    position:'absolute',
+    left:16,
+    top:24,
+    width:25,
+    height:35,
+  },
+  backImage: {
+    flex:1,
+    resizeMode:'contain'
+  },
+});
